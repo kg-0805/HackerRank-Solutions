9d8bf4ed5b4f1a4fcb9369c589297224bb204609
Kartik Gupta <hope.kartik@gmail.com>
2021-05-05 13:49:17 +0530

    Project Euler

---
+++ Project Euler/Project Euler #2 Even Fibonacci numbers/Solution.c
@@ -0,0 +1,26 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main() {
+    long int T, N;
+    long int A, B, C, evens_sum;
+    scanf("%ld", &T);
+    for (int _ = 0; _ < T; _++) {
+        scanf("%ld", &N);
+        A = 1; B = 2; C = 3;
+        evens_sum = 2;
+        while (C < N) {
+            if (C % 2 == 0) {
+                evens_sum += C;
+            }
+            A = B; B = C; C = A + B;
+        }
+        printf("%ld\n", evens_sum);
+    }
+    return 0;
+}
