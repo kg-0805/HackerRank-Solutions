03575af7d24ab2dc92f6b040af3f5207400b00c2
Kartik Gupta <hope.kartik@gmail.com>
2021-05-07 15:03:24 +0530

    Interview Preparation Kit

--- Interview Preparation Kit/Dictionaries and Hashmaps/Count Triplets/Solution.java
+++ Interview Preparation Kit/Dictionaries and Hashmaps/Count Triplets/Solution.java
@@ -1,60 +1,54 @@
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
+import java.text.*;
+import java.math.*;
 import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
 public class Solution {
 
-    // Complete the countTriplets function below.
-    private static long countTriplets(List<Long> arr, long r) {
-        Map<Long, Long> potential = new HashMap<>();
-        Map<Long, Long> counter = new HashMap<>();
-        long count = 0;
-        for (int i = 0; i < arr.size(); i++) {
-            long a = arr.get(i);
-            long key = a / r;
-            
-            if (counter.containsKey(key) && a % r == 0) {
-                count += counter.get(key);
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int arr[] = new int[n];
+            for(int arr_i=0; arr_i < n; arr_i++){
+                arr[arr_i] = in.nextInt();
             }
-            
-            if (potential.containsKey(key) && a % r == 0) {
-                long c = potential.get(key);
-                counter.put(a, counter.getOrDefault(a, 0L) + c);
-            }
-            
-            potential.put(a, potential.getOrDefault(a, 0L) + 1); // Every number can be the start of a triplet.
+            System.out.println(mergeSort(arr));
         }
+    }
+    
+    static long mergeSort(int[] a){
+        int temp[] = new int[a.length];
+        return mergeSort(a, temp, 0, a.length - 1);
+    }
+    
+    static long mergeSort(int[] a, int[] temp, int left, int right){
+        long count = 0;
+        if(right <= left) return 0;
+        int mid = left + (right - left) / 2;
+        count += mergeSort(a, temp, left, mid);
+        count += mergeSort(a, temp, mid + 1, right);
+        count += merge(a, temp, left, mid, right);
         return count;
     }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        String[] nr = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-        int n = Integer.parseInt(nr[0]);
-
-        long r = Long.parseLong(nr[1]);
-
-        List<Long> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-            .map(Long::parseLong)
-            .collect(toList());
-
-        long ans = countTriplets(arr, r);
-
-        bufferedWriter.write(String.valueOf(ans));
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
+    
+    static long merge(int[] a, int[] temp, int left, int mid, int right){
+        long count = 0;
+        for(int k = left; k <= right; k++){
+            temp[k] = a[k];
+        }
+        int i = left, j = mid + 1;
+        for(int k = left; k <= right; k++){
+            if(i > mid) a[k] = temp[j++];
+            else if(j > right) a[k] = temp[i++];
+            else if(temp[j] < temp[i]){
+                a[k] = temp[j++];
+                count += (mid - i + 1);
+            }
+            else a[k] = temp[i++];
+        }
+        return count;
     }
 }
